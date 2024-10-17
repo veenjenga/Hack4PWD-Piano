ac39d69a8a7c6e1117d2bdbe835745af9a696ac6
Sally Mutoko Namuseyi <sallynamuseyi@gmail.com>
2024-10-18 02:27:03 +0300

    FRONTEND

---
+++ products.js
@@ -0,0 +1,24 @@
+const products = [
+{
+    "id": "1",
+    "name": "art",
+    "price": 200,
+    "description": "dettgbrgd",
+    "image": "images/p3.png"
+},
+{
+    "id": "2",
+    "name": "art",
+    "price": 200,
+    "description": "dettgbrgd",
+    "image": "images/p4.png"
+},
+{
+    "id": "3",
+    "name": "art",
+    "price": 200,
+    "description": "dettgbrgd",
+    "image": "images/p5.png"
+}
+];
+export default products;
